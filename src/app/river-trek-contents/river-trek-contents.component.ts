a6d3f7b71238dd6427f3f97560d07d32e89dd70d
Admin <admin@Admins-MacBook-Pro.local>
2018-04-30 21:40:43 +0200

    all pages except map

---
+++ src/app/river-trek-contents/river-trek-contents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-river-trek-contents',
+  templateUrl: './river-trek-contents.component.html',
+  styleUrls: ['./river-trek-contents.component.css']
+})
+export class RiverTrekContentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
